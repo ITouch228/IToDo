8a700464b16f6a376500703b530eabee3acaf42a
ITouch228 <danilyatsenko200612354678@gmail.com>
2026-02-12 21:08:48 +0300

    fix: fixed frontend task endpoints slashes

--- frontend/src/api/tasks.js
+++ frontend/src/api/tasks.js
@@ -1,19 +1,19 @@
-import axios from "axios";
+import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL;
 
 axios.interceptors.response.use(
-  (response) => response,
-  (error) => {
+  response => response,
+  error => {
     if (error.response.status === 401) {
       window.location.reload();
-      alert("Сессия истекла, пожалуйста перезайдите в аккаунт");
+      alert('Сессия истекла, пожалуйста перезайдите в аккаунт');
     }
     return Promise.reject(error);
-  }
+  },
 );
 
-export const createTask = async (taskData) => {
+export const createTask = async taskData => {
   try {
     const formattedTask = {
       title: taskData.title,
@@ -22,7 +22,7 @@ export const createTask = async (taskData) => {
       priority: taskData.priority || 2,
       is_completed: taskData.is_completed || false,
     };
-    const token = localStorage.getItem("access_token");
+    const token = localStorage.getItem('access_token');
 
     const response = await axios.post(`${API_URL}/tasks/`, formattedTask, {
       headers: {
@@ -31,12 +31,12 @@ export const createTask = async (taskData) => {
     });
     return response.data;
   } catch (error) {
-    console.error("Task creation error:", JSON.stringify(error));
+    console.error('Task creation error:', JSON.stringify(error));
     throw error;
   }
 };
 
-export const editTask = async (taskData) => {
+export const editTask = async taskData => {
   try {
     const formattedTask = {
       id: taskData.id,
@@ -47,22 +47,22 @@ export const editTask = async (taskData) => {
       is_completed: taskData.is_completed || false,
     };
     console.log(formattedTask);
-    const token = localStorage.getItem("access_token");
+    const token = localStorage.getItem('access_token');
 
-    const response = await axios.post(`${API_URL}/tasks/edit/`, formattedTask, {
+    const response = await axios.post(`${API_URL}/tasks/edit`, formattedTask, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
     });
     return response.data;
   } catch (error) {
-    console.error("Task edit error:", JSON.stringify(error));
+    console.error('Task edit error:', JSON.stringify(error));
     throw error;
   }
-}
+};
 
 export const getTasks = async () => {
-  const token = localStorage.getItem("access_token");
+  const token = localStorage.getItem('access_token');
 
   if (token) {
     const response = await axios.get(`${API_URL}/tasks/`, {
@@ -74,37 +74,37 @@ export const getTasks = async () => {
   }
 };
 
-export const deleteTask = async (task) => {
+export const deleteTask = async task => {
   try {
-    const token = localStorage.getItem("access_token");
+    const token = localStorage.getItem('access_token');
 
-    const response = await axios.delete(`${API_URL}/tasks/${task.id}/`, {
+    const response = await axios.delete(`${API_URL}/tasks/${task.id}`, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
     });
     return response.data;
   } catch (error) {
-    console.error("Tasks delete error:", JSON.stringify(error));
+    console.error('Tasks delete error:', JSON.stringify(error));
     throw error;
   }
 };
 
 export const completeTask = async (taskId, newState) => {
   try {
-    const token = localStorage.getItem("access_token");
+    const token = localStorage.getItem('access_token');
 
     const response = await axios.post(
-      `${API_URL}/tasks/complete/${taskId}/${newState}/`,
+      `${API_URL}/tasks/complete/${taskId}/${newState}`,
       {
         headers: {
           Authorization: `Bearer ${token}`,
         },
-      }
+      },
     );
     return response.data;
   } catch (error) {
-    console.error("Tasks complete error:", JSON.stringify(error));
+    console.error('Tasks complete error:', JSON.stringify(error));
     throw error;
   }
 };
